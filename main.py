ccab6d6311e497972c7497c4eab6ddafec29619a
leila571 <leilamarks@postoakschool.org>
2020-10-19 10:50:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Leila Lopez Marks
+10/19/2020
+Battleship Code Planning
+"""
+def Introduction():
+    # Welcome message
+def Board():
+    # Lay out battleship game board
+def Controls():
+    # Allow players to move their icon up and down, left and right
+def Players():
+    # Have the players choose a color/team
+def Check_for_hits():
+    # check if the player hit anything
+def WinCondition():
+    # Who won?
+def WinMessage():
+    # show win message to whoever won
+    # ascii art
+    # ask to play again
+def Board_refresh():
+    # refresh board if players are playing again
+
